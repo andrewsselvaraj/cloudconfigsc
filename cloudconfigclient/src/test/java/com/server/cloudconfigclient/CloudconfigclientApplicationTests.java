4e481522c9d762092c745ad82c648367c127ea26
shiju <shiju@LAPTOP-QFQM3362>
2020-08-24 02:02:57 +0530

    Base version

---
+++ cloudconfigclient/src/test/java/com/server/cloudconfigclient/CloudconfigclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.cloudconfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudconfigclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
